862d047bb1bdbf25277ddca343dff81981f8a1bb
Michael Garcia <garciamichael.developer@gmail.com>
2021-12-26 16:05:14 -0700

    Start of project

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+# String concatenanation (how to put strings together)
+# Create a string that says "Subscribe to ____"
+youtuber = "Michael Garcia" #string variable
+
+#few ways to do this
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
