fdaed270c9023023317945b673068ef9c37b485c
Michael Garcia <garciamichael.developer@gmail.com>
2021-12-26 16:11:33 -0700

    Test of different ways to print

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,8 @@
-# String concatenanation (how to put strings together)
+# String concatenation (how to put strings together)
 # Create a string that says "Subscribe to ____"
 youtuber = "Michael Garcia" #string variable
 
 #few ways to do this
 print("Subscribe to " + youtuber)
 print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
